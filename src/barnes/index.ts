b3c1602e7b0ee0e8c51c6afc08cd4d5ef44f76e1
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2026-05-23 20:22:01 -0600

    don't need to wrap in main(); adjust n-s resolution downward to match current wx impl

--- src/barnes/index.ts
+++ src/barnes/index.ts
@@ -4,32 +4,31 @@ import { marchingSquares } from "../efficient/helpers.js";
 import { barnes, getBarnesParams } from "@plymer/fast-barnes-ts";
 import { readInputFromCsv, writeToGeoJson } from "./helpers.js";
 
-async function main() {
-  const pointTupleArray = await readInputFromCsv();
+const pointTupleArray = await readInputFromCsv();
 
-  const params = getBarnesParams(pointTupleArray, { mode: "euclidean", resolution: 2048 });
+const params = getBarnesParams(pointTupleArray, {
+  mode: "euclidean",
+  resolution: [2048, 2048 / 1.45],
+});
 
-  if (!params) {
-    throw new Error("Failed to compute Barnes parameters");
-  }
-
-  const { data, shape } = barnes(pointTupleArray, 1.5, params.x0, params.step, params.size, {
-    maxDist: 3.717,
-    numIter: 18,
-    method: "optimized_convolution",
-  });
-
-  // const thresholds = Array.from({ length: 25 }).map((_, i) => 960 + i * 4);
-
-  const thresholds = Array.from({ length: 21 }).map((_, i) => -50 + i * 5);
-
-  const polylines = marchingSquares(thresholds, data, shape as [number, number]);
-
-  console.log(
-    `Computed ${polylines.polylines.length} polylines for the following thresholds: ${polylines.levelValues.join(", ")}`,
-  );
-
-  await writeToGeoJson(polylines, params.x0, params.step);
+if (!params) {
+  throw new Error("Failed to compute Barnes parameters");
 }
 
-await main();
+const { data, shape } = barnes(pointTupleArray, 1.5, params.x0, params.step, params.size, {
+  maxDist: 3.717,
+  numIter: 18,
+  method: "optimized_convolution",
+});
+
+// const thresholds = Array.from({ length: 26 }).map((_, i) => 960 + i * 4);
+
+const thresholds = Array.from({ length: 21 }).map((_, i) => -50 + i * 5);
+
+const polylines = marchingSquares(thresholds, data, shape as [number, number]);
+
+console.log(
+  `Computed ${polylines.polylines.length} polylines for the following thresholds: ${polylines.levelValues.join(", ")}`,
+);
+
+await writeToGeoJson(polylines, params.x0, params.step);
